f264aa20d7d97c1863f79c948decf57435ae7b61
Kong ji han <wisejohn950330@gmail.com>
2022-07-07 22:42:48 +0900

    채팅서버 front 디자인 & js 이벤트 적용

---
+++ chat_front/js/chat.js
@@ -0,0 +1,33 @@
+function getSendMsgBox(msg){
+    return `<div class="sent_msg">
+    <p>${msg}</p>
+    <span class="time_date"> 11:18 | Today</span>
+    </div>`;
+}
+
+
+document.querySelector("#chat-send").addEventListener("click", ()=> {
+    let chatBox = document.querySelector("#chat-box");
+    let msgInput = document.querySelector("#chat-outgoing-msg");
+
+    let chatOutgoingBox = document.createElement("div");
+    chatOutgoingBox.className = "outgoing_msg";
+    
+    chatOutgoingBox.innerHTML = getSendMsgBox(msgInput.value);
+    chatBox.append(chatOutgoingBox);
+    msgInput.value = "";
+});
+
+document.querySelector("#chat-outgoing-msg").addEventListener("keydown", (e) => {
+    if(e.keyCode===13) {
+        let chatBox = document.querySelector("#chat-box");
+        let msgInput = document.querySelector("#chat-outgoing-msg");
+    
+        let chatOutgoingBox = document.createElement("div");
+        chatOutgoingBox.className = "outgoing_msg";
+        
+        chatOutgoingBox.innerHTML = getSendMsgBox(msgInput.value);
+        chatBox.append(chatOutgoingBox);
+        msgInput.value = "";
+    }
+});
